1a69283d8adc6d67928533b5cdf59e416aa0490b
Jitendra Yadav <jitendray144@gmail.com>
2021-07-21 01:02:31 +0530

    Time: 88 ms (91.62%), Space: 90.1 MB (98.77%) - LeetHub

---
+++ shuffle-an-array/shuffle-an-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    vector<int> temp;    
+public:
+    Solution(vector<int>& nums) {
+        temp = nums;        
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    vector<int> reset() {
+        return temp;
+    }
+    
+    /** Returns a random shuffling of the array. */
+    vector<int> shuffle() {
+        vector <int> temp_2;
+        temp_2 = temp;
+        
+        for(int i = 0;i < temp_2.size();i++){
+            
+            //  rand() generates random number. It can generate any number between 1 to temp.size() - 1;
+            int index = rand() % (i+1);
+            
+            // Swap to randomly shuffle the numbers.
+            swap(temp_2[i],temp_2[index]);
+        }
+        return temp_2;
+    }
+};
